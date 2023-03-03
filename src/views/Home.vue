41fccce0e1ed85774072a247f8a9529bb3a1253d
Y.Emre Durmuş <96531497+mredurmus@users.noreply.github.com>
2023-03-03 19:17:27 +0300

    update-1

---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <BlogPost :post="welcomeScreen"/>
+    <BlogPost :post="post" v-for="(post,index) in sampleBlogPost" :key="index"/>
+  </div>
+</template>
+
+<script>
+import BlogPost from "../components/BlogPost.vue"
+export default {
+  name: "Home",
+  components: {BlogPost},
+  data () {
+    return {
+      welcomeScreen: {
+        title: "Welcome",
+        blogPost: "For You 💖",
+        welcomeScreen: true,
+        photo: "lol",
+      },
+      sampleBlogPost: [
+        {
+          title: "This is a Filler Title!",
+          blogHTML: "This is a filler blog post title",
+          blogCoverPhoto: "beautiful-stories",
+        },
+        {
+          title: "This is a Filler Title2!",
+          blogHTML: "This is a filler blog post title",
+          blogCoverPhoto: "designed-for-everyone",
+        },
+        {
+          title: "This is a Filler Title3!",
+          blogHTML: "This is a filler blog post title",
+          blogCoverPhoto: "coding",
+        },
+      ],
+    };
+  },
+};
+</script>
